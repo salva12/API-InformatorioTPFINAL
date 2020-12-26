5cfa4ffba0123cd1dd0e6b4461cf4485a1994061
salva12 <salvamanu12@mail.com>
2020-12-26 20:56:33 -0300

    agregue mi coleccion de Postman, Primera version de la API concretada

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -3,12 +3,16 @@ INSERT INTO `infordb1`.`user` (`id`, `apellido`, `ciudad`, `email`, `fecha_creac
 INSERT INTO `infordb1`.`user` (`id`, `apellido`, `ciudad`, `email`, `fecha_creacion`, `nombre`, `pais`, `password`, `provincia`) VALUES ('12', 'Alfonso', 'Posadas', 'geral@gmail.com', '2020-12-22', 'German', 'Argentina', '15ger78', 'Misiones');
 INSERT INTO `infordb1`.`user` (`id`, `apellido`, `ciudad`, `email`, `fecha_creacion`, `nombre`, `pais`, `password`, `provincia`) VALUES ('2', 'Babugia', 'San Martin', 'babu@gmail.com', '2020-12-06', 'Florencia', 'Argentina', 'flor_cita', 'Chaco');
 INSERT INTO `infordb1`.`post` (`id`, `contenido`, `descripcion`, `fecha_creacion`, `publicado`, `titulo`, `autor`) VALUES ('3', 'Gozala, que la vida es una mami gozala, mira que este mundo da mil vueltas, ojala y no llegues a mi puerta por que no te abrire.', 'Cancion', '2020-12-18', TRUE, 'Parse', '2');
-INSERT INTO `infordb1`.`post` (`id`, `contenido`, `descripcion`, `fecha_creacion`, `publicado`, `titulo`, `autor`) VALUES ('8', 'La gratitud es la memoria del corazón.', 'Frase', '2020-12-15', FALSE, 'Gratitud', '4');
-INSERT INTO `infordb1`.`post` (`id`, `contenido`, `descripcion`, `fecha_creacion`, `publicado`, `titulo`, `autor`) VALUES ('12', '.¿Por qué estás hablando con esas zapatillas? .Porque pone converse!', 'Chiste', '2020-12-20', TRUE, 'Zapas Hablantes', '7');
+INSERT INTO `infordb1`.`post` (`id`, `contenido`, `descripcion`, `fecha_creacion`, `publicado`, `titulo`, `autor`) VALUES ('8', 'La gratitud es la memoria del corazon.', 'Frase', '2020-12-15', FALSE, 'Gratitud', '4');
+INSERT INTO `infordb1`.`post` (`id`, `contenido`, `descripcion`, `fecha_creacion`, `publicado`, `titulo`, `autor`) VALUES ('12', '¿Por que estas hablando con esas zapatillas? Porque pone converse!', 'Chiste', '2020-12-20', TRUE, 'Zapas Hablantes', '7');
 INSERT INTO `infordb1`.`user_posts` (`user_id`, `posts_id`) VALUES ('2', '3');
 INSERT INTO `infordb1`.`user_posts` (`user_id`, `posts_id`) VALUES ('4', '8');
 INSERT INTO `infordb1`.`user_posts` (`user_id`, `posts_id`) VALUES ('7', '12');
 INSERT INTO `infordb1`.`comentario` (`id`, `autor`, `comentario`, `fecha_creacion`, `post`) VALUES ('14', 'rombel@gmail.com', 'jajaja que gracioso!', '2020-12-21', '12');
 INSERT INTO `infordb1`.`comentario` (`id`, `autor`, `comentario`, `fecha_creacion`, `post`) VALUES ('17', 'landi@gmail.com', 'uhh temaikenn reina!', '2020-12-20', '3');
+INSERT INTO `infordb1`.`comentario` (`id`, `autor`, `comentario`, `fecha_creacion`, `post`) VALUES ('20', 'salvio@gmail.com', 'muy buen cancion', '2020-12-22', '3');
+INSERT INTO `infordb1`.`comentario` (`id`, `autor`, `comentario`, `fecha_creacion`, `post`) VALUES ('25', 'matflow@gmail.com', 'recomendadisima', '2020-12-23', '3');
 INSERT INTO `infordb1`.`post_comentarios` (`post_id`, `comentarios_id`) VALUES ('12', '14');
 INSERT INTO `infordb1`.`post_comentarios` (`post_id`, `comentarios_id`) VALUES ('3', '17');
+INSERT INTO `infordb1`.`post_comentarios` (`post_id`, `comentarios_id`) VALUES ('3', '20');
+INSERT INTO `infordb1`.`post_comentarios` (`post_id`, `comentarios_id`) VALUES ('3', '25');
